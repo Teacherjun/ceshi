5997b7850651fafd3f9b6342f6b9d07aa6fb3e69
guli <1215920101@qq.com>
2021-05-08 14:15:28 +0800

    Default Changelist

--- service/service-edu/src/main/java/com/atguigu/eduservice/controller/EduTeacherController.java
+++ service/service-edu/src/main/java/com/atguigu/eduservice/controller/EduTeacherController.java
@@ -3,6 +3,7 @@ package com.atguigu.eduservice.controller;
 
 import com.atguigu.eduservice.entity.EduTeacher;
 import com.atguigu.eduservice.service.EduTeacherService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,11 +24,13 @@ public class EduTeacherController {
     @Autowired
     private EduTeacherService teacherService;
 
+    @ApiOperation(value = "所有讲师列表")
     @GetMapping("findAll")
     public List<EduTeacher> list(){
         return teacherService.list(null);
     }
 
+    @ApiOperation(value = "根据ID删除讲师")
     @DeleteMapping("{id}")
     public boolean removeById(@PathVariable String id){
         return teacherService.removeById(id);
